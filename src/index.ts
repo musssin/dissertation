91b03f6501b43c9f3aa2b65f8cfd1a0461a2ae5c
Mussin Y <mussin0401@gmail.com>
2024-04-02 17:27:59 +0500

    static folder

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use(express.static('public'));
 app.use(httpContext.middleware);
 useExpressServer(app, {
   controllers: [SegmentationController],
